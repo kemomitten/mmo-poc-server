61d63acdb9b98f33f3121c3a1a29adadd2207fdd
Raikume <nakurakanra@hotmail.com>
2019-04-22 19:41:46 +0100

    Simple socket connection done, need to figure out how to handle multiple connections

---
+++ src/App.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import model.ServerProcessProtocol;
+
+public class App {
+
+	public static void main(String[] args) {
+		int portNumber = 9001;
+		
+		try{
+			ServerSocket serverSocket = new ServerSocket(portNumber);
+			Socket clientSocket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+			String inputLine, outputLine;
+			
+			ServerProcessProtocol sp = new ServerProcessProtocol();
+			//outputLine = sp.processInput(null);
+			//out.println(outputLine);
+			
+			
+			while(clientSocket.isConnected()) {
+				while((inputLine = in.readLine()) != null) {
+					outputLine = sp.processInput(inputLine);
+					out.println(outputLine);
+					if(outputLine.equals("Disconnect")) {
+						break;
+					}
+				}
+			}
+			
+		}catch(IOException e){
+			System.out.println("Exception caught when trying to listen on port "+portNumber+" or listening for a connection");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/model/ServerProcessProtocol.java
@@ -0,0 +1,19 @@
+package model;
+import java.util.HashMap;
+
+
+public class ServerProcessProtocol {
+	HashMap<String,String> ids = new HashMap<String,String>();
+	
+	
+	public String processInput(String input) {
+		String output = null;
+		System.out.println(input);
+		if(input.equals("something")) {
+			output = "You did a thing.";
+			
+		}
+		
+		return output;
+	}
+}
